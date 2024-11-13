830ec45550d6753c9e8588e94028ecc4f09e49a7
Gauthier Rigaux <rigauxgauthier@gmail.com>
2024-11-13 11:59:53 +0100

    chgmt migrations

--- migrations/Version20241108112040.php
+++ migrations/Version20241108112040.php
@@ -21,13 +21,13 @@ final class Version20241108112040 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, introduction VARCHAR(255) NOT NULL, picture VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE voiture CHANGE cover_image cover_image VARCHAR(255) DEFAULT NULL');
+        // $this->addSql('ALTER TABLE voiture CHANGE cover_image cover_image VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE user');
-        $this->addSql('ALTER TABLE voiture CHANGE cover_image cover_image VARCHAR(255) NOT NULL');
+        // $this->addSql('ALTER TABLE voiture CHANGE cover_image cover_image VARCHAR(255) NOT NULL');
     }
 }
